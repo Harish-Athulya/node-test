6cd614b05503eb8fdca821869f19c28cccd8d02b
Harish-Athulya <harishath88@gmail.com>
2022-11-11 17:01:27 +0530

    Third Message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 
-var port = 9000;
+// var port = 9000;
 
 app.get('/login/test', function(req, res) {
     console.log('receiving data ...');
@@ -13,7 +13,7 @@ app.get('/login/test', function(req, res) {
     res.send("Employee : Harish");
 });
 
-app.get('/login', function(req, res) {
+app.get('/login', '127.0.0.1', function(req, res) {
     console.log('receiving data ...');
     console.log('body is ',req.body);
     res.send("Employee : Harish");
@@ -21,8 +21,8 @@ app.get('/login', function(req, res) {
 
 
 // start the server  
-app.listen(port);
-console.log('Server started! At http://localhost:' + port);
+app.listen(process.env.PORT || 5000);
+// console.log('Server started! At http://localhost:' + port);
 
 
 
